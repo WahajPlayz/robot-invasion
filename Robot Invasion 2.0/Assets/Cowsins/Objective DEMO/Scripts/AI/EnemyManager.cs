f8a549d61fe6eb6680f084a8336188566551f17e
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2024-07-02 07:22:27 +0100

    Finished the floor of the boss room

--- Robot Invasion 2.0/Assets/Cowsins/Objective DEMO/Scripts/AI/EnemyManager.cs
+++ Robot Invasion 2.0/Assets/Cowsins/Objective DEMO/Scripts/AI/EnemyManager.cs
@@ -9,10 +9,12 @@ namespace Unity.FPS.AI
         public List<EnemyAI> Enemies { get; private set; }
         public int NumberOfEnemiesTotal { get; private set; }
         public int NumberOfEnemiesRemaining => Enemies.Count;
+        public List<cowsins.EnemyHealth> TargetEnemies { get; private set; }
 
         void Awake()
         {
             Enemies = new List<EnemyAI>();
+            TargetEnemies = new List<cowsins.EnemyHealth>();
         }
 
         public void RegisterEnemy(EnemyAI enemy)
@@ -23,6 +25,31 @@ namespace Unity.FPS.AI
                 Debug.LogWarning("The Enemy Manager already contain this enemy in it's list");
 
             NumberOfEnemiesTotal++;
+        }
+        
+        public void RegisterTarget(cowsins.EnemyHealth Target)
+        {
+            if (!TargetEnemies.Contains(Target))
+                TargetEnemies.Add(Target);
+            else
+                Debug.LogWarning("The Enemy Manager already contain this enemy in it's list");
+
+            NumberOfEnemiesTotal++;
+        }
+        public void UnregisterTarget(cowsins.EnemyHealth TargetKilled)
+        {
+            if (TargetEnemies.Contains(TargetKilled))
+            {
+                int enemiesRemainingNotification = NumberOfEnemiesRemaining - 1;
+
+                EnemyKillEvent evt = Unity.FPS.Game.Events.EnemyKillEvent;
+                evt.Enemy = TargetKilled.gameObject;
+                evt.RemainingEnemyCount = enemiesRemainingNotification;
+                EventManager.Broadcast(evt);
+
+                // removes the enemy from the list, so that we can keep track of how many are left on the map
+                TargetEnemies.Remove(TargetKilled);
+            }
         }
 
         public void UnregisterEnemy(EnemyAI enemyKilled)
