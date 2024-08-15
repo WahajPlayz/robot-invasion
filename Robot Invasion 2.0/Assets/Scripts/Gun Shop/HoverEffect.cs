5236d013dd69a6a1d88f046202a89ffb2bb97ae0
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2024-08-15 22:05:37 +0100

    add effect?

---
+++ Robot Invasion 2.0/Assets/Scripts/Gun Shop/HoverEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+
+    // pointer enters the UI element
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    //  pointer exits the UI element
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+// Clicked
+ public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+}
